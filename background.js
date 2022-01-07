716e53db0889609ec89f026c81cfb3c603bd6da8
caitlin <caitlinong@gmail.com>
2022-01-08 04:32:59 +0800

    more popups added

--- background.js
+++ background.js
@@ -17,7 +17,7 @@ const urlPatterns = [
 
 var masterPattern = new RegExp(urlPatterns.join('|'))
 
-const getActiveUrl = (tabid, changeInfo, tab) => {
+const getActiveUrl = ( changeInfo, tab) => {
     const url = changeInfo.url;
   
     // url is likely to be empty, and filter chrome:// and about:// URLs
@@ -32,5 +32,45 @@ const getActiveUrl = (tabid, changeInfo, tab) => {
         console.log("Stop Right There!")
     }
   }
-  
+
 chrome.tabs.onUpdated.addListener(getActiveUrl);
+
+function timer2() {
+    chrome.browserAction.setPopup({popup: "popup2.html"});
+  }
+
+  
+function timer3() {
+    chrome.browserAction.setPopup({popup: "popup3.html"});
+  }
+  
+function timer4() {
+    chrome.browserAction.setPopup({popup: "popup4.html"});
+  }
+  
+function timer5() {
+    chrome.browserAction.setPopup({popup: "popup5.html"});
+  }
+// startTimer = (duration, display) => {
+//     let time = duration, minutes, seconds;
+  
+  
+//   setInterval(() => {
+    
+//     minutes = parseInt(time / 60, 10);
+//     seconds = parseInt(time % 60, 10);
+//     console.log(seconds)
+//     minutes = minutes < 10 ? "0" + minutes : minutes;
+//     seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+//     display.innerHTML = `${minutes}:${seconds}`;
+//     if (--time < 0) {
+//       time = duration;
+//     }
+//    }, 1000);
+//   }
+  
+//   chrome.tabs.onUpdated.addListener = () => {
+//     let duration = 60 * .1, display = document.querySelector('.time');  
+//     startTimer(duration, display);
+//   }
